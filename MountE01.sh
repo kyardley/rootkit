5bb68ab7fa47d6ba99a44a5d3f1df5083f712896
Klynt Yardley <klynt.yardley@gmail.com>
2019-05-11 05:34:27 +0000

    added MountE01.sh

---
+++ MountE01.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+year=$(date +"%Y" )
+modulo=$(( $year % 2 ))
+
+if [ $modulo -eq 0 ]; then
+    echo "${year} is an even year"
+    echo "Looking for RootkitRootBeer_EvenYear.E01"
+    e01="RootkitRootBeer_EvenYear.E01"
+elif [ $modulo -eq 1 ]; then
+    echo "${year} is an odd year"
+    echo "Looking for RootkitRootBeer_OddYear.E01"
+    e01="RootkitRootBeer_OddYear.E01"
+else
+    echo "Failed to identify the current year; Please mount E01 file manually"
+    exit 1
+fi
+
+e01path="/home/sansforensics/Desktop/cases/$e01"
+if test -f $e01path; then
+    echo "$e01 found"
+else
+    echo "Couldn't find $e01 located at $e01path"
+fi
+
+echo "Now mounting E01 file"
+sudo ewfmount $e01path /mnt/ewf_mount/
+sudo mount -t ext4 -o ro,loop,noexec,noload,offset=1048576 /mnt/ewf_mount/ewf1 /mnt/e01
