834f0c145e0c8113cc78bd7abeb61ef216aad5c2
Klynt Yardley <klynt.yardley@gmail.com>
2019-05-11 16:25:29 +0000

    modified MountE01.sh

--- MountE01.sh
+++ MountE01.sh
@@ -20,8 +20,17 @@ if test -f $e01path; then
     echo "$e01 found"
 else
     echo "Couldn't find $e01 located at $e01path"
+    exit 1
 fi
 
 echo "Now mounting E01 file"
 sudo ewfmount $e01path /mnt/ewf_mount/
 sudo mount -t ext4 -o ro,loop,noexec,noload,offset=1048576 /mnt/ewf_mount/ewf1 /mnt/e01
+
+sudo touch /etc/init.d/rootkit-mount
+sudo chmod +x /etc/init.d/rootkit-mount
+echo "#!/bin/bash" >> /etc/init.d/rootkit-mount
+echo "sudo ewfmount $e01path /mnt/ewf_mount/" >> /etc/init.d/rootkit-mount
+echo "sudo mount -t ext4 -o ro,loop,noexec,noload,offset=1048576 /mnt/ewf_mount/ewf1 /mnt/e01" >> /etc/init.d/rootkit-mount
+
+sudo update-rc.d rootkit-mount defaults
